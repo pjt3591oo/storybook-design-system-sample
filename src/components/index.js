22301fb281a748f968c9845cdd813228b3b88af5
pjt3591oo <pjt3591oo@gmail.com>
2022-02-12 19:17:25 +0900

    readme 업데이트

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as PrimaryButton } from "./button/primary.component";
+export { default as SecondaryButton } from "./button/secondary.component";
+export { default as Title } from "./text/title.component";
+export { default as Tag } from "./tag/tag.component";
